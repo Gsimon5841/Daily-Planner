ddb0e3980d68712d76fb58650d7932bbaff37691
Remnant <georgesimon4801@gmail.com>
2022-01-31 19:02:06 -0500

    local storage

--- Develop/assets/js/scripts.js
+++ Develop/assets/js/scripts.js
@@ -1,8 +1,10 @@
 var tasks = {}
 
-console.log (moment().format('llll'));
+console.log(moment().format('llll'));
 $("#currentDay").append(moment().format('llll'));
-$(".task").on("click", "p", function(){
+
+
+$(".task").on("click", "p", function () {
     var text = $(this).text().trim();
     console.log(text);
     var textInput = $("<textarea>").addClass("task-control").val(text);
@@ -11,24 +13,55 @@ $(".task").on("click", "p", function(){
 
 });
 
-$(".save").on("click", "button", function(){
-   var saveEl = "Button";
-   if (saveEl === "Button") {
-       var text = this.parentElement.previousElementSibling.children[0].value
-       console.log(text)
-       var status = $(this)
-       .closest(".taskContainer")
-       .attr("id")
-       .replace("task-", "")
-       console.log(status)
-      
-       var index = $(this)
-       .closest(".taskContainer").index();
-       console.log(index)
+$(".save").on("click", "button", function () {
+    var saveEl = "Button";
+    if (saveEl === "Button") {
+        console.log(this.parentElement.previousElementSibling)
+        var text = this.parentElement.previousElementSibling.value
+        console.log(text)
+        var status = $(this)
+            .closest(".taskContainer")
+            .attr("id")
 
-       tasks[status][index].text = text;
-      
-      
-   }
+        console.log(status)
+
+        var index = $(this)
+            .closest(".taskContainer").index();
+        console.log(index)
+
+        localStorage.setItem(status,text,index);
+
+
+
+
+    }
 })
-console.log(moment().format("H"))
+
+function saveTask(){
+    $("#9 .task").val(localStorage.getItem("9"))
+    for (i = 9; i<=17; i++) {
+        $("#" + i + " .task").val(localStorage.getItem( i + ""));
+        
+    }
+}
+
+function trackHour() {
+    var currentTime = moment().format("H");
+    for (var i = 9; i <= 17; i++) {
+        var id = document.getElementById(i).children[1]
+
+        if (i < currentTime) {
+            id.classList.add("past")
+
+        } else if (i == currentTime) {
+            id.classList.add("present")
+        } else if (i > currentTime) {
+            id.classList.add("future")
+        }
+
+
+    }
+
+}
+trackHour()
+saveTask()
